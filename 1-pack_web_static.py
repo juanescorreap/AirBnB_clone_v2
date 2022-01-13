95f5c8da9dbb28e5a85b2baf94e145ba79557b3d
John Doe <juanestebancope@outlook.com>
2022-01-13 16:48:24 -0500

    pep8 style

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Fabric script that generates a .tgz archive from the contents of
 the web_static folder of your AirBnB Clone repo, using the function do_pack."""
-import fabric.api
-import datetime
+from fabric.api import local
+from datetime import datetime
 
 
 def do_pack():
@@ -10,10 +10,10 @@ def do_pack():
     the web_static folder of your AirBnB Clone repo,
     using the function do_pack."""
 
-    fabric.api.local("mkdir -p versions")
+    local("mkdir -p versions")
     date = datetime.now().strftime("%Y%m%d%H%M%S")
     tgz_file = "versions/web_static_{}.tgz".format(date)
-    execute_tar = fabric.api.local("tar -cvzf {} web_static".format(tgz_file))
+    execute_tar = local("tar -cvzf {} web_static".format(tgz_file))
 
     if execute_tar.succeeded:
         return tgz_file
