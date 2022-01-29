74c608e0c8e62b1e1dec444472670de318115fe3
John Doe <juanestebancope@outlook.com>
2022-01-29 14:58:35 -0500

    pep8 style

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -17,9 +17,11 @@ def hello():
 def hbnb():
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def display_c(text):
     return "C {}".format(text.replace("_", " "))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
