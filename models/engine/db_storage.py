479fd5e2266847b61f30333dd19a72d93ff39ee6
John Doe <juanestebancope@outlook.com>
2022-01-30 15:01:01 -0500

    updates for deserializing the JSON file to objects and closing session

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -78,3 +78,7 @@ class DBStorage():
         session_factory = sessionmaker(
             bind=self.__engine, expire_on_commit=False)
         self.__session = scoped_session(session_factory)
+
+    def close(self):
+        """Closes session"""
+        self.__session.close()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -67,3 +67,7 @@ class FileStorage:
             self.save()
         else:
             pass
+
+    def close(self):
+        """method for deserializing the JSON file to objects"""
+        self.reload()
