f5a7459905bd122cf1975570ae8f1b3481a63a03
John Doe <juanestebancope@outlook.com>
2022-01-30 17:58:05 -0500

    test

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -10,16 +10,14 @@ app = Flask(__name__)
 
 
 @app.route('/cities_by_states', strict_slashes=False)
-def display_cities():
-	""" Fetches data from the storage engine"""
-	return render_template('8-cities_by_states.html',
-                       display=storage.all(State).values())
+def display_states():
+    return render_template('8-cities_by_states.html',
+                           display=storage.all(State).values())
 
 
 @app.teardown_appcontext
 def close_alchemy(self):
-    """Closes the current SQLAlchemy Session"""
     storage.close()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
